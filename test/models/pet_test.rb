6e8c3997e0430eb421d56d534426003a262b77a0
Stheffany <stheffanyhadlich@gmail.com>
2019-05-20 16:39:49 -0300

    models tests

--- test/models/pet_test.rb
+++ test/models/pet_test.rb
@@ -1,7 +1,49 @@
 require 'test_helper'
 
 class PetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "create a pet" do
+    assert_difference 'Pet.all.count', 1 do
+      Pet.create breed: 'Foo', size: 'Big', phone: '999999'
+    end
+  end
+
+  test "don't create a pet without breed" do
+    assert_difference 'Pet.all.count', 0 do
+      Pet.create size: 'Big', phone: '999999'
+    end
+  end
+
+  test "don't create a pet without size" do
+    assert_difference 'Pet.all.count', 0 do
+      Pet.create breed: 'Foo', phone: '999999'
+    end
+  end
+
+  test "don't create a pet without phone" do
+    assert_difference 'Pet.all.count', 0 do
+      Pet.create breed: 'Foo', size: 'Big'
+    end
+  end
+
+  test "don't create a pet with breed lenght < 3" do
+    assert_difference 'Pet.all.count', 0 do
+      Pet.create breed: 'A', size: 'Big', phone: '999999'
+    end
+  end
+
+  test "don't create a pet with size lenght < 3" do
+    assert_difference 'Pet.all.count', 0 do
+      Pet.create breed: 'Foo', size: 'B', phone: '999999'
+    end
+  end
+
+  test "don't create a pet with phone lenght < 3" do
+    assert_difference 'Pet.all.count', 0 do
+      Pet.create breed: 'Foo', size: 'Big', phone: '9'
+    end
+  end
+
+
+
 end
