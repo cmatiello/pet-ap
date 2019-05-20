b47f74e2a04bba883d886d58fcf83d463471bd1d
Stheffany <stheffanyhadlich@gmail.com>
2019-05-20 16:25:41 -0300

    controllers test

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class PetsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get pets_path
+    assert_response :success
+  end
+
+  test "should get show" do
+    @pet = Pet.create! breed: 'Foo', size: 'Big', phone: '9999999'
+    get pet_path(@pet.id)
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_pet_path
+    assert_response :success
+  end
+
+  test "should get edit" do
+    @pet = Pet.create! breed: 'Foo', size: 'Big', phone: '9999999'
+    get edit_pet_path(@pet.id)
+    assert_response :success
+  end
 end
