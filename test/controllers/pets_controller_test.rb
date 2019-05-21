5e5045e3c6ef6b83fd7acd262dae9a6755779088
Stheffany <stheffanyhadlich@gmail.com>
2019-05-21 14:26:23 -0300

    Post and Put tests

--- test/controllers/pets_controller_test.rb
+++ test/controllers/pets_controller_test.rb
@@ -22,4 +22,18 @@ class PetsControllerTest < ActionDispatch::IntegrationTest
     get edit_pet_path(@pet.id)
     assert_response :success
   end
+
+  test "should create a pet" do
+    assert_difference 'Pet.all.count', 1 do
+      post pets_path, params: {pet: {breed: 'Foo', size: 'Big', phone: '999999'}, format: "json"}
+    end
+  end
+
+  test "should edit a pet" do
+    @pet = Pet.create breed: 'Foo', size: 'Big', phone: '999999'
+    put "/pets/#{@pet.id}", params: {pet: {breed: "Bar"}, format: "json"}
+    result = Pet.find(@pet.id).breed
+    assert_equal(result, 'Bar')
+  end
+
 end
