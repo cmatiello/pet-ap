7393657899dae4b64633ff85e45134f099b1576a
Stheffany <stheffanyhadlich@gmail.com>
2019-05-29 22:37:30 -0300

    Add images

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,10 +1,11 @@
 class Pet < ApplicationRecord
- validates :breed, presence: true,
-                    length: { minimum: 3 }
+  has_one_attached :picture
+  validates :breed, presence: true,
+                      length: { minimum: 3 }
 
- validates :size, presence: true,
-                    length: { minimum: 3 }
+  validates :size, presence: true,
+                      length: { minimum: 3 }
 
- validates :phone, presence: true,
-                    length: { minimum: 3 }
+  validates :phone, presence: true,
+                      length: { minimum: 3 }
 end
